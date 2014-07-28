95990993d0865cd3bdb99a5467540c0e7b6f3d74
bwalters21 <bwalt9934@ung.edu>
2014-07-28 18:56:09 -0400

    Homework 2- emailed assignment

---
+++ HW-2.py
@@ -0,0 +1,16 @@
+def word_bank (word):
+    """randomly selects a string from a word bank"""
+    return r 
+
+
+def backwards (word):
+    """input word an the word is returned backwards"""
+    return word[::-1]
+
+
+def word (word):
+    """input = tacos, output = T _ _ _ _ """
+    underscores = len(word) - 1
+    return word[0] + "_ " * underscores
+
+
